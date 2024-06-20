5a1839a9e855da0adea39cf124815efa2d87f843
Barath <devarajanbarath@gmail.com>
2024-06-20 20:08:23 +0530

    initial commit

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id:1,
+    rating:10,
+    text:'<p>Lorem Ipsum ebfiuerfvuegfehbferhbkebiugurhfekbveihih</p>'
+  },
+  {
+    id:2,
+    rating:9,
+    text:'Lorem ipsum weyuwefewuguj'
+  },
+  {
+    id:3,
+    rating:8,
+    text:'Lorem ipsum ajgduwbejdfuewg'
+  },
+]
+
+export default FeedbackData
