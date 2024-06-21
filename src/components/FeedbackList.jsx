c371411def3e00e2c9efb0221799433e7b716cfd
Barath <devarajanbarath@gmail.com>
2024-06-21 14:23:35 +0530

    initial commit

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -3,15 +3,16 @@ import { useContext } from "react"
 import React from 'react'
 import FeedbackItems from "./FeedbackItems"
 import FeedbackContext from "../context/FeedbackContext"
+import Spinner from "./shared/Spinner"
 
 function FeedbackList() {
-  const {feedback} = useContext(FeedbackContext)
+  const {feedback, isLoading} = useContext(FeedbackContext)
 
-  if(!feedback || feedback.length === 0){
+  if(!isLoading && (!feedback || feedback.length === 0)){
     return <p>No feedback yet</p>
   }
 
-  return(
+  return isLoading ? <Spinner /> : (
     <div className="feedback-list">
       <AnimatePresence>
         {feedback.map((item) => (
@@ -26,7 +27,7 @@ function FeedbackList() {
         ))}
       </AnimatePresence>
     </div>
-    )
+  )
 
   // return(
   // <div className="feedback-list">
